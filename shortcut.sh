35930f6e4eb70ed3abe8cd498c139c23cceb9049
Samuel Li <git@samuelj.li>
2020-06-22 17:16:27 -0400

    Removed debug code

--- shortcut.sh
+++ shortcut.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
-echo "test" > debug.txt
 xclip -out -selection primary | xclip -in -selection clipboard
 SELECTED=$(xsel --clipboard | tr "\n" " ")
 python3 client.py $SELECTED
